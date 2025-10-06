23f30b3116ec0149fbd8008f410fc8df9ebe882f
apiqueres <alejandropiqueres01@gmail.com>
2025-10-06 13:54:34 +0200

    pagina en construccion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UnderConstructionComponent } from './under-construction/under-construction';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: UnderConstructionComponent },
+    { path: '**', redirectTo: '' }
+];
