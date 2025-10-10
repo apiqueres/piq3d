ada7f2d1e2f68835d2a6f331a447839e40ac504c
apiqueres <alejandropiqueres01@gmail.com>
2025-10-11 01:06:48 +0200

    config de routes

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -21,9 +21,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 4470, hash: 'b62c578fe205787859816a321554e635e98543f6f4b641956bbb689f45bbf337', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 4606, hash: '97a607fa90873efe8e9c8656a7457f35ee8e4b015b930afc0bb9634882d2a7e6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'inicio/index.html': {size: 45667, hash: 'f1153bacccf635acf95086b1d7e28b860a917bc38a62599389291cc745c751a4', text: () => import('./assets-chunks/inicio_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 4449, hash: '4fcbd98155ade548b6b4f76e8756c3ce7262774e3bb691c265ca71f568facd1a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 4592, hash: '3a4bb15f81424cbbfccfe2f8f16d82090e2d07e22f44cabbe9c470f14e7a3eef', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'inicio/index.html': {size: 45646, hash: 'f7ae4b31843a3ce26d0699776cd9db33694a724a9b85fdfcc38d3cfaa40b2dfb', text: () => import('./assets-chunks/inicio_index_html.mjs').then(m => m.default)},
     'styles-FC6WLD7C.css': {size: 405, hash: 'NGwIDlNzeI0', text: () => import('./assets-chunks/styles-FC6WLD7C_css.mjs').then(m => m.default)}
   },
 };
