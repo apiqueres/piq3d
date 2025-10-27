4146d57ff208ae4208b529578032d7950c984730
apiqueres <alejandropiqueres01@gmail.com>
2025-10-27 13:42:11 +0100

    correccion inicio

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -34,12 +34,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 58629, hash: '7d071563a33b703e696aa9c51b3ad03ba93ef8f22082bf24cb4b9e13a9e0613b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 4926, hash: 'f0b8ec173a62802c00de52d9d92cbd3db03ded5b4374fb99260b414f4ba80649', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'inicio/index.html': {size: 77736, hash: '55bf34900244c75f218fab3d541b568d77be6f80bc93612d4e8c74153bc162ea', text: () => import('./assets-chunks/inicio_index_html.mjs').then(m => m.default)},
-    'gracias/index.html': {size: 67932, hash: 'b7c6e39421c0fe6fc9972bb31d6326b42050883fb755c39dce5a9f9b19d10d22', text: () => import('./assets-chunks/gracias_index_html.mjs').then(m => m.default)},
-    'contacto/index.html': {size: 76632, hash: '1aecd289228526ad2afc93b00b91a738341034ceacd80cc3b81e35230c8e81e2', text: () => import('./assets-chunks/contacto_index_html.mjs').then(m => m.default)},
-    'productos/index.html': {size: 80766, hash: 'eaf2f536415e8bbc6c1ca3442bce94995167e8451e85fa92b4a1df2ec50541e5', text: () => import('./assets-chunks/productos_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 58629, hash: '08005e499246af127e16ee773ba92a82e44cfdc6683636399653ffd12b014241', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 4926, hash: 'ce7a7acbd4d74abc81aeae4e4805d7b3201cae7058bcbfb0b3bf9eca6ee3399b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'inicio/index.html': {size: 77732, hash: '7d371b71d79694f3fc2304a41db14f120a9af25a3cda8a2bc1ff3a6cb353ee9b', text: () => import('./assets-chunks/inicio_index_html.mjs').then(m => m.default)},
+    'gracias/index.html': {size: 67932, hash: '1a66196f28f6b46db24fc1392063e49807546dbe8cf807a179f04a0ccf40de94', text: () => import('./assets-chunks/gracias_index_html.mjs').then(m => m.default)},
+    'contacto/index.html': {size: 76632, hash: 'c9bb5fa76c1a9dfdb28d67379fc3ecaf5bc8251e74e0390b634d5d71a8b110cb', text: () => import('./assets-chunks/contacto_index_html.mjs').then(m => m.default)},
+    'productos/index.html': {size: 80766, hash: '47f438999c8538dd1f21adbdb137bd7384ab6cd3af0fbadb596329613ea4f7e7', text: () => import('./assets-chunks/productos_index_html.mjs').then(m => m.default)},
     'styles-EEKN4ZKZ.css': {size: 130321, hash: 'RtYMvnomXlc', text: () => import('./assets-chunks/styles-EEKN4ZKZ_css.mjs').then(m => m.default)}
   },
 };
